b8b2bb91cd009bb539cf9edfae6f14e465e33d1a
ygorrc <ygormagalhaesrc@gmail.com>
2021-08-25 21:54:00 -0300

    Projeto finalizado
    Funções de comida e aumentar cobra acabada
    
    condição para terminar o jogo incrementada

--- js/script.js
+++ js/script.js
@@ -6,24 +6,58 @@ cobra[0] = {
     x: 8 * box,
     y: 8 * box
 }
+let comida = {
+    x: Math.floor(Math.random() * 15 + 1) * box,
+    y: Math.floor(Math.random() * 15 + 1) * box
+}
 
 let direcao = "right";
 function criarBG(){
     context.fillStyle = "lightgreen";
     context.fillRect(0, 0, 16*box, 16*box); 
 }
-
+// função de criação da cobrinha , com origem no meio do tabuleiro
 function criarCobrinha(){
     for(i=0; i< cobra.length; i++){
         context.fillStyle = "green";
-        context.fillRect(cobra[i] .x, cobra[i].y, box, box);
+        context.fillRect(cobra[i].x, cobra[i].y, box, box);
     }
 }
 
+function droparComida(){
+    context.fillStyle = "red";
+    context.fillRect(comida.x, comida.y, box, box); 
+}
+// pega os cliques que a pessoa da no teclado e passa para função atualizar
+document.addEventListener('keydown', atualizar);
+
+
+// função para atualizar a nova direção da cobra de acordo com o que usuario digita
+function atualizar(event){
+    if(event.keyCode == 37 && direcao != "right") direcao = "left";
+    if(event.keyCode == 38 && direcao != "down") direcao = "up";
+    if(event.keyCode == 39 && direcao != "left") direcao = "right";
+    if(event.keyCode == 40 && direcao != "up") direcao = "down";
+}
+
+
 function iniciarJogo(){
+    
+    if(cobra[0].x > 15 * box && direcao == "right") cobra[0].x = 0;
+    if(cobra[0].x < 0 && direcao == "left") cobra[0].x = 16 * box;
+    if(cobra[0].y > 15 * box && direcao == "down") cobra[0].y = 0;
+    if(cobra[0].y < 0 && direcao == "up") cobra[0].y = 16 * box;
+
+    for(i = 1; i < cobra.length; i++){
+        if(cobra[0].x == cobra[i].x && cobra[0].y == cobra[i].y){
+            clearInterval(jogo);
+            alert("Game Over");
+        }
+    }
     
     criarBG();
     criarCobrinha();
+    droparComida();
 
 
     let cobraX = cobra[0].x;
@@ -35,7 +69,13 @@ function iniciarJogo(){
     if(direcao == "up") cobraY -= box;
     if(direcao == "down") cobraY += box;
 
-    cobra.pop();
+    if(cobraX != comida.x || cobraY != comida.y){
+        cobra.pop();
+    }
+    else{
+        comida.x = Math.floor(Math.random() * 15 + 1) * box;
+        comida.y = Math.floor(Math.random() * 15 + 1) * box;
+    }
     
     let novaCabeca = {
         x: cobraX,
